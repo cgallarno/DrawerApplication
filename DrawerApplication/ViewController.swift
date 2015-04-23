78b17ddd9e778b135dbe3eaba3acddea870c1346
Cameron Gallarno <cgallarno@gmail.com>
2015-04-23 12:43:09 -0400

    Empty App

---
+++ DrawerApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawerApplication
+//
+//  Created by Cameron Gallarno on 4/23/15.
+//  Copyright (c) 2015 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
